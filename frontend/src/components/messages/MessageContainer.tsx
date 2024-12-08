b6cd089d3a4516d643fe6f0a930fae6b264abb9d
saatvik-10 <saatvikmadan529@gmail.com>
2024-12-08 15:05:05 +0530

    classname fixes

--- frontend/src/components/messages/MessageContainer.tsx
+++ frontend/src/components/messages/MessageContainer.tsx
@@ -6,8 +6,8 @@ const MessageContainer = () => {
     <div className="flex flex-col md:min-w-[450px]">
       <>
         {/* Header */}
-        <div className="mb-2 bg-slate-500 px-4 py-2">
-          <span className="label-text">To:</span>{" "}
+        <div className="mb-2 flex items-center gap-1 bg-slate-500 px-4 py-2">
+          <span className="label-text">To:</span>
           <span className="font-bold text-gray-900">John doe</span>
         </div>
 
