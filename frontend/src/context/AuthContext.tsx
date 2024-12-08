394f01a01a76b9e92b253744edfe537ae47228fd
saatvik-10 <saatvikmadan529@gmail.com>
2024-12-08 19:52:11 +0530

    fixes

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -65,6 +65,7 @@ export const AuthContextProvider = ({
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
   return useContext(AuthContext);
 };
