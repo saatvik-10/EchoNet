fd1a4931bc4ba7cadeb9dd3c454f664d9ee508e6
saatvik-10 <saatvikmadan529@gmail.com>
2024-12-20 22:55:52 +0530

    fixes in msgs

--- frontend/src/components/messages/Messages.tsx
+++ frontend/src/components/messages/Messages.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import useGetMsgs from "../../hooks/conversation/useGetMsgs";
 import useListenMsg from "../../hooks/conversation/useListenMsg";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
@@ -6,28 +5,17 @@ import Message from "./Message";
 
 const Messages = () => {
   const { loading, messages } = useGetMsgs();
-  const [showSkeleton, setShowSkeleton] = useState(true);
 
   useListenMsg();
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSkeleton(false);
-    }, 1000); // 1 second delay
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="flex-1 overflow-auto px-4">
-      {showSkeleton &&
-        loading && [...Array(3).map((_, idx) => <MessageSkeleton key={idx} />)]}
+      {loading && [...Array(3).map((_, idx) => <MessageSkeleton key={idx} />)]}
 
-      {!showSkeleton &&
-        !loading &&
+      {!loading &&
         messages.map((msg) => <Message key={msg.id} message={msg} />)}
 
-      {!showSkeleton && !loading && messages.length === 0 && (
+      {!loading && messages.length === 0 && (
         <p className="text-center text-white">
           Send a message to start a conversation...
         </p>
