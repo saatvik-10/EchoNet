93c605af4dc6cc60cf606d74c9037e3e564a2666
saatvik-10 <saatvikmadan529@gmail.com>
2024-12-21 14:03:27 +0530

    fixes in path

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,17 +7,17 @@ import { serveStatic } from '@hono/node-server/serve-static';
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
 app.route('/api/auth', authRoutes); //for signin and signup
 app.route('/api/messages', msgRoutes); //for posting and getting msg
 
 if (process.env.NODE_ENV === 'production') {
+  const staticPath = path.join(__dirname, '../../frontend/dist');
+
   // Serve static files from the frontend/dist directory
-  app.use('/*', serveStatic({ root: './frontend/dist' }));
+  app.use('/*', serveStatic({ root: staticPath }));
 
   // Catch-all route to serve index.html for client-side routing
-  app.get('*', (c) => c.html('./frontend/dist/index.html'));
+  app.get('*', (c) => c.html(path.join(staticPath, 'index.html')));
 }
 
 // Start listening
