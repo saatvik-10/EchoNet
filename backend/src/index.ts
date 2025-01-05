f456c607a9f48766dc52e32dd19645a11c2d0946
saatvik-10 <saatvikmadan529@gmail.com>
2025-01-05 20:24:44 +0530

    changes to file structure

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,7 @@
-import {config} from 'dotenv';
-import {resolve} from 'path';
+import 'dotenv/config';
 import authRoutes from './routes/auth.route';
 import msgRoutes from './routes/msg.route';
 import { app, server } from './socket/socket';
-
-config({ path: resolve(__dirname, '../.env') })
 
 const PORT = process.env.PORT || 5000;
 
@@ -20,6 +17,10 @@ app.route('/api/messages', msgRoutes); //for posting and getting msg
 //   // Catch-all route to serve index.html for client-side routing
 //   app.get('*', (c) => c.html(path.join(staticPath, 'index.html')));
 // }
+
+app.get('/', async (c) => {
+  return c.text('Hello World');
+});
 
 // Start listening
 server.listen(PORT, () => {
